38efc5b744fcbfc3d6065f61290d78a42628c671
cubeiplkumar <emailanilkumarm@gmail.com>
2024-02-01 18:11:07 +0530

    feature2.2 changes done

--- Application.java
+++ Application.java
@@ -59,6 +59,25 @@ public class GenericListVersion2 implements Iterable{
         for(Object obj : c){
             this.add(obj);
         }
-    }
+    #feature2.2 changes by vinay on SavingsAccount Module on Mar 2023
+	// create a method
+  public static int square(int num) {
+
+    // return statement
+    return num * num;
+  }
+
+  public static void main(String[] args) {
+    int result;
+
+    // call the method
+    // store returned value to result
+    result = square(10);
+
+    System.out.println("Squared value of 10 is: " + result);
+  }
+}
+	
+	
     
     
